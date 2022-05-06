28629f8cb7dd5b28794075fca5fb788858ffa087
Kevin Tang <Kevin.Tang8895@gmail.com>
2022-05-06 11:41:03 +0200

    sidebar ergänzt mit icons

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app =express();
 const port=3000;
 
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname+'/index.html')
+    res.sendFile(__dirname+'/layouts/index.html')
 
+})
+
+app.listen(port, ()=>{
+    console.log(`Port ${port} wird ausgeführt`)
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,17 @@ module.exports = {
     './layouts/*.html',
     // './layout/*.js'
   ],
-  darkMode:'class',
+  darkMode:'class', //false, class, media
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'crete-round': ['"Crete Round"', 'serif']
+      },
+      boxShadow: {
+      'tool': '0 5px 10px rgba(0, 0 , 0, 0.3)',
+      }
+
+    },
   },
   plugins: [],
 }
